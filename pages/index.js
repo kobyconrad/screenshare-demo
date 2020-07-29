b397bba70bb1861e2e6f9c2698f533cd1d917507
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-07-29 15:10:07 -0700

    set up basic UI

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,39 @@
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div className="appContainer">
+      <div
+        className="shareButton"
+        onClick={() => {
+          console.log("click");
+        }}
+      >
+        START SHARE
+      </div>
+      <style jsx>{`
+        .appContainer {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          height: 100vh;
+        }
+        .shareButton {
+          background-color: #444;
+          width: 160px;
+          height: 40px;
+          border-radius: 4px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          color: white;
+          font-size: 18px;
+          font-weight: bold;
+          cursor: pointer;
+        }
+        .shareButton:hover {
+          opacity: 0.7;
+          cursor: pointer;
+        }
+      `}</style>
+    </div>
+  );
 }
